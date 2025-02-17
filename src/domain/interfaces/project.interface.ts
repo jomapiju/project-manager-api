a932e8ea5489beeedec9447484bd6ace0ce2f09c
jmjunior <jmjunior@icatuseguros.com.br>
2025-02-17 20:04:44 -0300

    aula 1

---
+++ src/domain/interfaces/project.interface.ts
@@ -0,0 +1,10 @@
+import { IUser } from "./user.interface";
+import { ITask } from "./task.interface";
+
+export class IProject {
+    id: number;
+    name: string;
+    description: string;
+    tasks: ITask[];
+    user: IUser;
+}
